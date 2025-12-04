375bc69f8da6a6bef1d1ab257d79eeed0c523114
Igor Todorovic <igor333m@yahoo.com>
2025-12-04 14:23:20 -0800

    Add json.db to the phonebook

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import type { Person } from '../types/person'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-
-
-  const [persons, setPersons] = useState<Person[]>([
-    { name: 'Arto Hellas', number: '39-44-9956', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState<Person[]>([])
   const [filteredPersons, setFilteredPersons] = useState<Person[]>([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then(res => res.json())
+      .then(json => setPersons(json))
+      .catch(err => console.error(err));
+  }, [])
 
 
   const nameUpdate = (e: any) => {
