e17f049e0372aaf84cf8d46d55c76cb61ed7ea29
Igor Todorovic <igor333m@yahoo.com>
2025-12-10 10:52:12 -0800

    Update weather icon and interface

--- part2/countries/src/components/Weather.tsx
+++ part2/countries/src/components/Weather.tsx
@@ -1,22 +1,32 @@
 import { useEffect, useState } from 'react'
 import weatherService from '../services/weather'
 
+interface WeatherData {
+  weather?: Array<{ icon: string }>;
+  main?: { temp: number };
+  wind?: { speed: number };
+}
+
 const Weather = ({country}: any) => {
 
   const api_key = import.meta.env.VITE_WEATHER
 
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState<WeatherData>({} as WeatherData)
   const [icon, setIcon] = useState('')
 
   const getWeather = async () => {
     try {
-      const data = await weatherService.get(country.latlng[0], country.latlng[1], api_key)
+      const data: WeatherData = await weatherService.get(country.latlng[0], country.latlng[1], api_key)
       setWeather(data)
 
       const iconCode = data?.weather?.[0]?.icon
       if (iconCode) {
         const iconUrl = await weatherService.getIcon(iconCode)
-        setIcon(iconUrl)
+        if (typeof iconUrl === 'string') {
+          setIcon(iconUrl)
+        } else {
+          setIcon('')
+        }
       } else {
         setIcon('')
       }
@@ -33,7 +43,7 @@ const Weather = ({country}: any) => {
     <section>
       <h3>Weather in {country.capital[0]}</h3>
       <p>Temperature {weather?.main?.temp} Celsius</p>
-      <img src={icon} alt='Current weather icon' />
+      {icon && <img src={icon} alt='Current weather icon' /> }
       <p>Wind {weather?.wind?.speed} m/s</p>
     </section>
   )
